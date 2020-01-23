3b719730af61389b4ac5158d16fe5583ee001eec
jkpraha <shinjawkwang@naver.com>
2020-01-23 14:17:17 +0000

    added favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,6 +9,14 @@ module.exports = {
 	description: 'personal technology wiki',
     base: "/techArchive/",
     dest: "build",
+    head: [
+        ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/favicons/apple-touch-icon.png"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/favicons/favicon-32x32.png"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/favicons/favicon-16x16.png"}],
+        ['link', { rel: "manifest", href: "/assets/favicons/manifest.json"}],
+        ['link', { rel: "shortcut icon", href: "/assets/favicons/favicon.ico"}],
+        ['meta', { name: "msapplication-config", content: "/assets/favicons/browserconfig.xml"}],
+    ],
 	markdown: {
 		anchor: {
 			permalink: true, permalinkBefore: true, permalinkSymbol: octicons.link.toSVG()
